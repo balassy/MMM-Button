f0f7a1e2d6f42eb06b1b8ea7978dd644a9a78a02
György Balássy <balassy@users.noreply.github.com>
2018-07-27 01:47:45 +0200

    FIXED: Code cleanup.

--- MMM-Button.js
+++ MMM-Button.js
@@ -1,4 +1,4 @@
-/* global Module */
+/* global Module, Log */
 
 /* Magic Mirror
  * Module: MMM-Button
@@ -6,22 +6,23 @@
  * MIT Licensed.
  */
 
-Module.register('MMM-Button',{	
-	requiresVersion: "2.1.0",
-	defaults: {
-		buttonPIN: 5,
-		notificationMessage: "BUTTON_PRESSED",
-		//time in miliseconds before another button click is recognized
-		clickDelay: 500,		
-	},	
-	// Override socket notification handler.
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === this.config.notificationMessage){
-			this.sendNotification(this.config.notificationMessage, payload)
-		}
-	},	
-	start: function() {
-		this.sendSocketNotification('BUTTON_CONFIG', this.config);
-		Log.info('Starting module: ' + this.name);
-	}
+Module.register('MMM-Button', {
+  requiresVersion: "2.1.0",
+
+  defaults: {
+    buttonPIN: 5,
+    notificationMessage: "BUTTON_PRESSED",
+    clickDelay: 500 // Time in miliseconds before another button click is recognized.
+  },
+
+  start: function () {
+    this.sendSocketNotification('BUTTON_CONFIG', this.config);
+    Log.info('Starting module: ' + this.name);
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === this.config.notificationMessage) {
+      this.sendNotification(this.config.notificationMessage, payload)
+    }
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -7,30 +7,30 @@
  */
 
 const NodeHelper = require('node_helper');
-const gpio = require('onoff');
+const GPIO = require('onoff').Gpio;
+
 module.exports = NodeHelper.create({
-  start: function () {
+  start() {
     this.started = false
   },
-  // Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, payload) {
+
+
+  socketNotificationReceived(notification, payload) {
     const self = this;
-    if (notification === 'BUTTON_CONFIG' && this.started == false) {     
-		const self = this
-		this.config = payload     	  
-		var GPIO = require('onoff').Gpio,
-		button = new GPIO(this.config.buttonPIN, 'in', 'both',{ persistentWatch: true, debounceTimeout: this.config.clickDelay });
-		button.watch(function(err, state) {
-		  // check the state of the button
-		  // 1 == pressed, 0 == not pressed
-		  if(state == 1) {
-			// send notification for broadcast
-			self.sendSocketNotification(this.config.notificationMessage, true);
-			console.log("button pressed");
-			}
-		});          
-		this.started = true
+    if (notification === 'BUTTON_CONFIG' && this.started === false) {
+      this.config = payload;
+
+      const button = new GPIO(this.config.buttonPIN, 'in', 'both', { persistentWatch: true, debounceTimeout: this.config.clickDelay });
+      button.watch(function (err, state) {
+        // check the state of the button
+        // 1 == pressed, 0 == not pressed
+        if (state == 1) {
+          self.sendSocketNotification(self.config.notificationMessage, true);
+          console.log("button pressed");
+        }
+      });
+
+      this.started = true
     };
   }
-  
 });
